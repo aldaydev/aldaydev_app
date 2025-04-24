cbe94a60e48147bc5373975ebaf8094d642c6b80
aldaydev <rafaldayparejo@gmail.com>
2025-04-24 14:14:26 +0200

    key enter menuToggle updated

--- src/components/Header/MenuToggle.jsx
+++ src/components/Header/MenuToggle.jsx
@@ -1,14 +1,25 @@
 import { forwardRef } from 'react';
 import './menuToggle.css';
 
-const MenuToggle = forwardRef( ({isOpen, setIsOpen, ...props}, ref) => {
+const MenuToggle = forwardRef(({ isOpen, setIsOpen, ...props }, ref) => {
 
     // const [isOpen, setIsOpen] = useState(false);
 
     const toggleClass = isOpen ? "menuToggle__container--open" : "menuToggle__container--close";
 
     return (
-        <div className={`menuToggle__container ${toggleClass}`} onClick={() => setIsOpen(!isOpen)} ref={ref} aria-label={isOpen ? "Cerrar menú" : "Abrir menú"} {...props}>
+        <div
+            className={`menuToggle__container ${toggleClass}`}
+            onClick={() => setIsOpen(!isOpen)}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {  // Detecta Enter o Espacio
+                    setIsOpen(!isOpen);
+                }
+            }}
+            ref={ref}
+            aria-label={isOpen ? "Cerrar menú" : "Abrir menú"}
+            {...props}
+        >
             <div className="menuToggle__bar menuToggle__bar--1"></div>
             <div className="menuToggle__bar menuToggle__bar--2"></div>
             <div className="menuToggle__bar menuToggle__bar--3"></div>
