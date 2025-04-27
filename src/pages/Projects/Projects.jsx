266418c476222e81040b4669613ea61035138c87
aldaydev <rafaldayparejo@gmail.com>
2025-04-27 16:36:37 +0200

    collecting icons

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,21 @@
 import AnimatedTitle from '../../components/AnimatedTitle/AnimatedTitle';
 import './projects.css';
 
-// import projects_bg from '../../../assets/images/projects_bg';
+import technologiesData from '../../data/technologiesData.js';
+import { useState, useEffect } from 'react';
 
 function Projects () {
+
+    const [techFilterList, setTechFilterList] = useState([]);
+
+    useEffect(()=>{
+        const techFilterItems = technologiesData.filter((techItem)=>{
+            return techItem.isFilter === true;
+        })
+        setTechFilterList(techFilterItems);
+    },[])
+
+
     return(
         <>
             <section className='projects'>
@@ -11,13 +23,27 @@ function Projects () {
                     <AnimatedTitle 
                         headingLevel = "h1"
                         initialText="P"
-                        finalText="rojects"
+                        finalText="royectos"
                         id="page__animatedTitle"
                         fontSize="3rem"
                         braceSize="3.4rem"
                         color="var(--color-white)"
                     />
                 </header>
+
+                <section>
+                    {techFilterList && techFilterList.map( (techFilterItem) => {
+                        console.log(techFilterItem.icon)
+                        return (
+                            <label>
+                                <input type='checkbox'/>
+                                {/* <img src={`../..${techFilterItem.icon}`}/> */}
+                                <img src={techFilterItem.icon} style={{height: "40px", filter: techFilterItem.textColor === 'black' ? "invert(0)" : "invert(1)"}}/>
+                                <span style={{fontSize: "1.7rem"}}>{techFilterItem.name}</span>
+                            </label>
+                        )
+                    })}
+                </section>
                 
             </section>
         </>
