deb78d37eb1d1d7176123c5cc014d85ccffd384c
aldaydev <rafaldayparejo@gmail.com>
2025-05-07 20:28:38 +0200

    working on resume navbar

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -8,6 +8,7 @@ import ResumeProjects from './components/ResumeProjects';
 import ResumeExperience from './components/ResumeExperience';
 
 import cv from '../../assets/documents/cv-rafa-alday.pdf';
+import { useEffect } from 'react';
 
 function Resume() {
 
@@ -18,51 +19,68 @@ function Resume() {
         "Experiencia"
     ]
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const resumeNavbar = document.querySelector(".resumeHeader__navbarList");
+
+            if (scrollTop > 120) {
+                resumeNavbar.classList.add("resumeHeader__navbarList--scroll");
+            } else if (scrollTop < 80) {
+                resumeNavbar.classList.remove("resumeHeader__navbarList--scroll");
+            }
+
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [window.scrollY]);
+
 
     return (
         <div className='pageContainer resume'>
-                <AnimatedTitle
-                    headingLevel="h1"
-                    initialText="C"
-                    finalText="urrículum"
-                    id="page__animatedTitle"
-                    fontSize="3rem"
-                    braceSize="3.4rem"
-                    color="var(--color-white)"
-                />
-                <nav className="resumeHeader__navbar">
-                    
-                    <ul className="resumeHeader__navbarList">
-                        
-                        {
-                            resumeSections && resumeSections.map((resumeSection, index) => {
-                                return (
-                                    <li key={index} className="resumeHeader__navbarItem">
-                                        <span className='resumeHeader__bg'/>
-                                        <a
-                                            href="#"
-                                            className="resumeHeader__navbarLink"
-                                        >
-                                            {resumeSection}
-                                        </a>
-                                    </li>
-                                )
-                            })
-                        }
+            <AnimatedTitle
+                headingLevel="h1"
+                initialText="C"
+                finalText="urrículum"
+                id="page__animatedTitle"
+                fontSize="3rem"
+                braceSize="3.4rem"
+                color="var(--color-white)"
+            />
+            <nav className="resumeHeader__navbar">
 
-                    </ul>
-                </nav>
+                <ul className="resumeHeader__navbarList">
+
+                    {
+                        resumeSections && resumeSections.map((resumeSection, index) => {
+                            return (
+                                <li key={index} className="resumeHeader__navbarItem">
+                                    <span className='resumeHeader__bg' />
+                                    <a
+                                        href="#"
+                                        className="resumeHeader__navbarLink"
+                                    >
+                                        {resumeSection}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+
+                </ul>
+            </nav>
 
             <section className='resume__content'>
-                <ResumePresentation/>
-                <ResumeTechnologies/>
-                <ResumeProjects/>
-                <ResumeEducation/>
-                <ResumeExperience/>
+                <ResumePresentation />
+                <ResumeTechnologies />
+                <ResumeProjects />
+                <ResumeEducation />
+                <ResumeExperience />
             </section>
 
             <section className='resume__download'>
-                <a 
+                <a
                     href={cv}
                     target='_blank' rel="noopener noreferrer"
                     className='resume__downloadLink'
