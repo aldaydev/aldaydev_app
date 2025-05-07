35f0fcbf5c8931c8885f426df82f10ca42890806
aldaydev <rafaldayparejo@gmail.com>
2025-05-07 16:27:11 +0200

    working on resume projects

--- src/pages/Resume/components/ResumeProjects.jsx
+++ src/pages/Resume/components/ResumeProjects.jsx
@@ -1,5 +1,7 @@
 import './resumeProjects.css';
 
+import React from 'react';
+
 import projects from '../../../data/projectsData.js';
 
 import github_icon from '../../../assets/icons/util-icons/github_simpleicon.svg';
@@ -13,59 +15,93 @@ function ResumeProjects() {
         <article className="resume__projects">
             <h3 className="resume__articleTitle resume__projectsTitle">PROYECTOS DESTACADOS</h3>
             <div className='resume__resumeProjects'>
-                {
-                    mainProjects && mainProjects.map((project, index) => {
-                        return (
-                            <div key={index}>
-                                <figure>
-                                    <img src={project.img.desktop} />
-                                </figure>
-                                <div>
-                                    <h4>{project.name}</h4>
-                                    <h5>{project.date}</h5>
-                                    <p>{project.description}</p>
-                                    <div>
-                                        <h5>Tecnologías: </h5>
-                                        <p>
+                <div className='resumeProjects__projects'>
+                    {
+                        mainProjects && mainProjects.map((project, index) => {
+                            return (
+                                <div key={index} className='resumeProjects__mainProject'>
+                                    <div className='mainProject__imageContainer'>
+                                        <img
+                                            src={project.img.desktop}
+                                            className='mainProject__image'
+                                        />
+
+                                        <div className='mainProject__linksContainer'>
                                             {
-                                                project.technologies.map((tech, index) => {
+                                                project.github.map((githubLink, index) => {
                                                     return (
-                                                        <span key={index}>{tech.toUpperCase()}</span>
+
+                                                        <a
+                                                            key={index}
+                                                            href={githubLink.url}
+                                                            target='_blank'
+                                                            rel="noopener noreferrer"
+                                                            className='mainProject__link'
+                                                        >
+                                                            <img
+                                                                src={github_icon}
+                                                                className='mainProject__linkIcon'
+                                                            />
+                                                            <span className='mainProject__linkText'>
+                                                                {githubLink.type.toUpperCase()}
+                                                            </span>
+                                                        </a>
+
                                                     )
                                                 })
                                             }
-                                        </p>
-                                        
+                                            {
+                                                project.deployed &&
+                                                <a
+                                                    href={project.deployed}
+                                                    target='_blank'
+                                                    rel="noopener noreferrer"
+                                                    className='mainProject__link'
+                                                >
+                                                    <img
+                                                        src={web_icon}
+                                                        className='mainProject__linkIcon'
+                                                    />
+                                                    <span className='mainProject__linkText'>
+                                                        DESPLEGADO
+                                                    </span>
+                                                </a>
+                                            }
+                                        </div>
                                     </div>
-                                    <div>
-                                        {
-                                            project.github.map((githubLink, index) => {
-                                                return (
-                                                    <div key={index}>
-                                                        <a href={githubLink.url}>
-                                                            <img src={github_icon} />
-                                                            <span>{githubLink.type.toUpperCase()}</span>
-                                                        </a>
+                                    <div className='mainProject__info'>
+                                        <div className='mainProject__titles'>
+                                            <h4 className='mainProject__title'>{project.name}</h4>
+                                            <h5 className='mainProject__date'>{project.date}</h5>
+                                        </div>
+                                        <p className='mainProject__description'>{project.description}</p>
+                                        <div className='mainProject__techContainer'>
+                                            <h5 className='mainProject__techTitle'>TECNOLOGÍAS: </h5>
+                                            <ul className='mainProject__techList'>
+                                                {
+                                                    project.technologies.map((tech, index) => (
+                                                        <React.Fragment key={index}>
+                                                            {index !== 0 && <span>|</span>}
+                                                            <li className='mainProject__techItem'>
+                                                                {tech.toUpperCase()}
+                                                            </li>
+                                                        </React.Fragment>
+                                                    ))
+                                                }
+                                            </ul>
 
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                    <div>
-                                        {
-                                            project.deployed &&
-                                            <a href={project.deployed}>
-                                                <img src={web_icon} />
-                                                <span>DESPLEGADO</span>
-                                            </a>
-                                        }
+                                        </div>
+
+                                        <div>
+
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
+
             </div>
         </article>
     )
